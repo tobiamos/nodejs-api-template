a7190b1abf68c1a16685b55c90ea6d1a97fae40f
tobidev <tobi@gokada.ng>
2018-04-03 11:55:48 +0100

    add and configure chalk for terminal styling

---
+++ src/config/chalk.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+
+module.exports.error = chalk.bold.red;
+module.exports.warning = chalk.keyword('orange');
+module.exports.blue = chalk.magenta;
+module.exports.green = chalk.green;
+module.exports.red = chalk.red;
