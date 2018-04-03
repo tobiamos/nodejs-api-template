278ded12dd63ede8d4e55f7d6b1b6530f88c791e
tobidev <tobi@gokada.ng>
2018-04-03 11:59:53 +0100

    add and configure winston for logging

---
+++ src/config/logger.js
@@ -0,0 +1,13 @@
+const winston = require('winston');
+
+const logger = new winston.Logger({
+  transports: [
+    new winston.transports.Console({
+      level: 'debug',
+      handleExceptions: true,
+      json: false,
+      colorize: true,
+    }),
+  ],
+  exitOnError: false,
+});
