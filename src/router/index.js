dbb64d0b4eafd7be899687d3ff654d5a36e97312
tobidev <tobi@gokada.ng>
2018-05-13 20:52:55 +0100

    feat: register user routes with the entire application

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const userRoutes = require('../modules/User/routes');
 
 const router = express.Router();
+
+router.use('/user', userRoutes);
 
 module.exports = router;
